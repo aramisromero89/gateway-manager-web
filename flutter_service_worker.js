be265a7dcd4bc8071c6a5b3a2a9685a579e9e7fd
aramisromero89 <aramisromero89@gmail.com>
2022-10-11 13:56:43 +0000

    update

---
+++ flutter_service_worker.js
@@ -0,0 +1,209 @@
+'use strict';
+const MANIFEST = 'flutter-app-manifest';
+const TEMP = 'flutter-temp-cache';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "main.dart.js_1.part.js": "962af0edcf5c680026eafeac27deb06b",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"main.dart.js": "9798ec7fb54104e40ffd28bc3e27a8a2",
+"splash/style.css": "cbfe62a2c7e6af0e29b484d30610f3cf",
+"splash/img/dark-4x.png": "2a09d6edf9c012dd4f7f042ec915acb9",
+"splash/img/dark-1x.png": "ca40b88009bf4a1cb52961f41b9b2508",
+"splash/img/light-1x.png": "64f083b664c38b95d0713cdd2aa91106",
+"splash/img/light-4x.png": "6ac1ae9c80642cbe22d08c67bf06ec17",
+"splash/img/light-3x.png": "0c57b0ed0baea8b307f373423ba13071",
+"splash/img/dark-3x.png": "76d1cb34d1fa5685a0aefda0339cd1d0",
+"splash/img/light-2x.png": "bdd78dd50b08a66c326cd67adf513b80",
+"splash/img/dark-2x.png": "487b6a68f15108929c550483edb1bab7",
+"splash/splash.js": "123c400b58bea74c1305ca3ac966748d",
+"index.html": "82b8674663a8bf64d7e1d94487c2e082",
+"/": "82b8674663a8bf64d7e1d94487c2e082",
+"flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
+"manifest.json": "79e201ab90d4ab6bfeb3abae3b5d497c",
+"canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
+"canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba",
+"canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
+"canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
+"assets/AssetManifest.json": "222bc785bdb07236eb8746bafcb02592",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
+"assets/packages/fluttertoast/assets/toastify.js": "e7006a0a033d834ef9414d48db3be6fc",
+"assets/packages/fluttertoast/assets/toastify.css": "a85675050054f179444bc5ad70ffc635",
+"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
+"assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
+"assets/NOTICES": "7800aff06f9014780a555b5d507dd2b5",
+"version.json": "872bf3b93ff82f23e4923d106e3fbeb4",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
+"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea"
+};
+
+// The application shell files that are downloaded before a service worker can
+// start.
+const CORE = [
+  "main.dart.js",
+"index.html",
+"assets/NOTICES",
+"assets/AssetManifest.json",
+"assets/FontManifest.json"];
+// During install, the TEMP cache is populated with the application shell files.
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  return event.waitUntil(
+    caches.open(TEMP).then((cache) => {
+      return cache.addAll(
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
+    })
+  );
+});
+
+// During activate, the cache is populated with the temp files downloaded in
+// install. If this service worker is upgrading from one with a saved
+// MANIFEST, then use this to retain unchanged resource files.
+self.addEventListener("activate", function(event) {
+  return event.waitUntil(async function() {
+    try {
+      var contentCache = await caches.open(CACHE_NAME);
+      var tempCache = await caches.open(TEMP);
+      var manifestCache = await caches.open(MANIFEST);
+      var manifest = await manifestCache.match('manifest');
+      // When there is no prior manifest, clear the entire cache.
+      if (!manifest) {
+        await caches.delete(CACHE_NAME);
+        contentCache = await caches.open(CACHE_NAME);
+        for (var request of await tempCache.keys()) {
+          var response = await tempCache.match(request);
+          await contentCache.put(request, response);
+        }
+        await caches.delete(TEMP);
+        // Save the manifest to make future upgrades efficient.
+        await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+        return;
+      }
+      var oldManifest = await manifest.json();
+      var origin = self.location.origin;
+      for (var request of await contentCache.keys()) {
+        var key = request.url.substring(origin.length + 1);
+        if (key == "") {
+          key = "/";
+        }
+        // If a resource from the old manifest is not in the new cache, or if
+        // the MD5 sum has changed, delete it. Otherwise the resource is left
+        // in the cache and can be reused by the new service worker.
+        if (!RESOURCES[key] || RESOURCES[key] != oldManifest[key]) {
+          await contentCache.delete(request);
+        }
+      }
+      // Populate the cache with the app shell TEMP files, potentially overwriting
+      // cache files preserved above.
+      for (var request of await tempCache.keys()) {
+        var response = await tempCache.match(request);
+        await contentCache.put(request, response);
+      }
+      await caches.delete(TEMP);
+      // Save the manifest to make future upgrades efficient.
+      await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+      return;
+    } catch (err) {
+      // On an unhandled exception the state of the cache cannot be guaranteed.
+      console.error('Failed to upgrade service worker: ' + err);
+      await caches.delete(CACHE_NAME);
+      await caches.delete(TEMP);
+      await caches.delete(MANIFEST);
+    }
+  }());
+});
+
+// The fetch handler redirects requests for RESOURCE files to the service
+// worker cache.
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  var origin = self.location.origin;
+  var key = event.request.url.substring(origin.length + 1);
+  // Redirect URLs to the index.html
+  if (key.indexOf('?v=') != -1) {
+    key = key.split('?v=')[0];
+  }
+  if (event.request.url == origin || event.request.url.startsWith(origin + '/#') || key == '') {
+    key = '/';
+  }
+  // If the URL is not the RESOURCE list then return to signal that the
+  // browser should take over.
+  if (!RESOURCES[key]) {
+    return;
+  }
+  // If the URL is the index.html, perform an online-first request.
+  if (key == '/') {
+    return onlineFirst(event);
+  }
+  event.respondWith(caches.open(CACHE_NAME)
+    .then((cache) =>  {
+      return cache.match(event.request).then((response) => {
+        // Either respond with the cached resource, or perform a fetch and
+        // lazily populate the cache.
+        return response || fetch(event.request).then((response) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      })
+    })
+  );
+});
+
+self.addEventListener('message', (event) => {
+  // SkipWaiting can be used to immediately activate a waiting service worker.
+  // This will also require a page refresh triggered by the main worker.
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+    return;
+  }
+  if (event.data === 'downloadOffline') {
+    downloadOffline();
+    return;
+  }
+});
+
+// Download offline will check the RESOURCES for all files not in the cache
+// and populate them.
+async function downloadOffline() {
+  var resources = [];
+  var contentCache = await caches.open(CACHE_NAME);
+  var currentContent = {};
+  for (var request of await contentCache.keys()) {
+    var key = request.url.substring(origin.length + 1);
+    if (key == "") {
+      key = "/";
+    }
+    currentContent[key] = true;
+  }
+  for (var resourceKey of Object.keys(RESOURCES)) {
+    if (!currentContent[resourceKey]) {
+      resources.push(resourceKey);
+    }
+  }
+  return contentCache.addAll(resources);
+}
+
+// Attempt to download the resource online before falling back to
+// the offline cache.
+function onlineFirst(event) {
+  return event.respondWith(
+    fetch(event.request).then((response) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    }).catch((error) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(event.request).then((response) => {
+          if (response != null) {
+            return response;
+          }
+          throw error;
+        });
+      });
+    })
+  );
+}
---
+++ main.dart.js_1.part.js
@@ -0,0 +1,686 @@
+self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
+$__dart_deferred_initializers__.current=function(a,b,c,$){var B={Yc:function Yc(d,e){this.a=d
+this.b=e},RI:function RI(d,e,f){this.c=d
+this.d=e
+this.a=f},E5:function E5(d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,a0){var _=this
+_.c=d
+_.d=e
+_.e=f
+_.f=g
+_.r=h
+_.w=i
+_.x=j
+_.y=k
+_.z=l
+_.Q=m
+_.as=n
+_.at=o
+_.ay=p
+_.ch=q
+_.CW=r
+_.cx=s
+_.cy=t
+_.db=u
+_.dx=v
+_.dy=w
+_.fr=x
+_.a=a0},E6:function E6(d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s){var _=this
+_.d=d
+_.e=!1
+_.iG$=e
+_.iH$=f
+_.jM$=g
+_.qP$=h
+_.qQ$=i
+_.nV$=j
+_.qR$=k
+_.nW$=l
+_.wi$=m
+_.nX$=n
+_.l0$=o
+_.l1$=p
+_.cQ$=q
+_.aN$=r
+_.a=null
+_.b=s
+_.c=null},amr:function amr(d){this.a=d},amo:function amo(d,e){this.a=d
+this.b=e},ams:function ams(d){this.a=d},amp:function amp(d,e){this.a=d
+this.b=e},amq:function amq(d){this.a=d},amt:function amt(d,e){this.a=d
+this.b=e},Fr:function Fr(d){var _=this
+_.p1=_.ok=_.k4=_.k3=_.k2=_.k1=_.id=_.go=_.fy=_.fx=_.fr=_.dy=_.dx=_.db=_.cy=null
+_.p2=!1
+_.at=_.as=_.Q=_.z=_.y=_.x=_.w=_.r=_.f=_.e=_.d=_.c=_.b=_.a=null
+_.to$=0
+_.x1$=d
+_.xr$=_.x2$=0
+_.y1$=!1},Gf:function Gf(){},Gg:function Gg(){},M0:function M0(d){this.a=d},KR:function KR(d,e,f){this.a=d
+this.b=e
+this.c=f},a2K:function a2K(d,e,f,g){var _=this
+_.d=d
+_.a=e
+_.b=f
+_.c=g},r9:function r9(d,e,f){this.d=d
+this.e=e
+this.a=f},TP:function TP(d,e,f){var _=this
+_.w=$
+_.x=d
+_.z=_.y=!1
+_.Q=e
+_.d=$
+_.a=null
+_.b=f
+_.c=null},ak_:function ak_(d,e){this.a=d
+this.b=e},ak5:function ak5(d){this.a=d},ak3:function ak3(d){this.a=d},ak2:function ak2(d,e){this.a=d
+this.b=e},ak4:function ak4(d,e,f){this.a=d
+this.b=e
+this.c=f},ak0:function ak0(d){this.a=d},ak1:function ak1(d){this.a=d},xC:function xC(d,e){this.d=d
+this.a=e},TQ:function TQ(d){var _=this
+_.d=_.w=$
+_.a=null
+_.b=d
+_.c=null},ake:function ake(d,e){this.a=d
+this.b=e},akc:function akc(d){this.a=d},akd:function akd(d){this.a=d},akf:function akf(d){this.a=d},akb:function akb(d,e){this.a=d
+this.b=e},ak9:function ak9(d,e,f){this.a=d
+this.b=e
+this.c=f},ak7:function ak7(d){this.a=d},ak8:function ak8(d){this.a=d},aka:function aka(d,e){this.a=d
+this.b=e},ak6:function ak6(d){this.a=d},yl:function yl(d,e){this.d=d
+this.a=e},UD:function UD(d){var _=this
+_.d=_.w=$
+_.a=null
+_.b=d
+_.c=null},al7:function al7(d){this.a=d},al5:function al5(d,e){this.a=d
+this.b=e},al3:function al3(d){this.a=d},al4:function al4(d){this.a=d},al6:function al6(d){this.a=d},
+aKU(d,e){return new B.ov(d,e)},
+ov:function ov(d,e){this.e=d
+this.a=e},
+LM:function LM(d,e,f,g){var _=this
+_.a=d
+_.b=e
+_.c=f
+_.d=g},
+aAd(d){var x
+d.X(y.y)
+x=A.ah(d)
+return x.eR}},A,D,C,J
+B=a.updateHolder(c[3],B)
+A=c[0]
+D=c[4]
+C=c[2]
+J=c[1]
+B.Yc.prototype={
+j(d){return"_SwitchType."+this.b}}
+B.RI.prototype={
+a2k(d){var x=A.ah(d)
+B.aAd(d)
+switch(x.f.a){case 0:return D.a_G
+case 1:return D.a_F}},
+a_u(d){var x=null
+return new B.E5(this.c,this.d,x,x,x,x,x,x,x,x,x,x,C.aV,x,x,x,x,x,x,!1,this.a2k(d),x)},
+I(d,e){switch(0){case 0:return this.a_u(e)}}}
+B.E5.prototype={
+V(){return new B.E6(new B.Fr($.aW()),$,$,$,$,$,$,$,$,$,null,!1,!1,null,null,C.k)}}
+B.E6.prototype={
+aV(d){var x,w=this,v="_position"
+w.bz(d)
+if(d.c!==w.a.c){x=A.b(w.iH$,v)
+if(x.gq(x)!==0){x=A.b(w.iH$,v)
+x=x.gq(x)===1}else x=!0
+if(x){x=A.b(w.iH$,v)
+x.b=C.aI
+x.c=C.d5}w.vg()}},
+l(d){this.d.l(0)
+this.WV(0)},
+gh5(){this.a.toString
+return this.ga2E()},
+gFb(){return!1},
+gq(d){return this.a.c},
+gBM(){return new A.cF(new B.amr(this),y.s)},
+gzJ(){var x,w=this.c
+w.toString
+x=A.ah(w)
+return new A.cF(new B.amo(x.as.a===C.ak,x),y.t)},
+gMJ(){return new A.cF(new B.ams(this),y.s)},
+gIs(){var x=this.c
+x.toString
+return new A.cF(new B.amp(this,A.ah(x).as.a===C.ak),y.t)},
+a8W(d){if(this.gh5()!=null)A.b(this.jM$,"_reactionController").bW(0)},
+a8Y(d){var x,w,v=this,u="_positionController"
+if(v.gh5()!=null){x=A.b(v.iH$,"_position")
+x.b=C.E
+x.c=null
+x=d.c
+x.toString
+w=x/(v.a.fr.a-40)
+x=v.c.X(y.I)
+x.toString
+switch(x.f.a){case 0:x=A.b(v.iG$,u)
+x.sq(0,A.b(x.x,"_value")-w)
+break
+case 1:x=A.b(v.iG$,u)
+x.sq(0,A.b(x.x,"_value")+w)
+break}}},
+a8U(d){var x,w,v=this,u=A.b(v.iH$,"_position")
+u=u.gq(u)
+x=v.a
+w=x.c
+if(u>=0.5!==w){x.d.$1(!w)
+v.O(new B.amq(v))}else v.vg()
+A.b(v.jM$,"_reactionController").dg(0)},
+a2F(d){var x=this.a.d
+d.toString
+x.$1(d)},
+I(a1,a2){var x,w,v,u,t,s,r,q,p,o,n,m,l,k,j,i,h,g,f,e,d=this,a0=null
+if(d.e){d.e=!1
+d.vg()}x=A.ah(a2)
+w=B.aAd(a2)
+v=d.geZ()
+v.E(0,C.aX)
+u=d.geZ()
+u.C(0,C.aX)
+d.a.toString
+t=d.gBM().a.$1(v)
+if(t==null){t=w.a
+t=t==null?a0:t.M(v)
+s=t}else s=t
+if(s==null)s=d.gzJ().a.$1(v)
+d.a.toString
+t=d.gBM().a.$1(u)
+if(t==null){t=w.a
+t=t==null?a0:t.M(u)
+r=t}else r=t
+if(r==null)r=d.gzJ().a.$1(u)
+d.a.toString
+t=d.gMJ().a.$1(v)
+if(t==null){t=w.b
+t=t==null?a0:t.M(v)
+q=t}else q=t
+if(q==null)q=d.gIs().a.$1(v)
+d.a.toString
+t=d.gMJ().a.$1(u)
+if(t==null){t=w.b
+t=t==null?a0:t.M(u)
+p=t}else p=t
+if(p==null)p=d.gIs().a.$1(u)
+o=d.geZ()
+o.E(0,C.aw)
+d.a.toString
+t=w.e
+n=t==null?a0:t.M(o)
+m=n
+if(m==null)m=x.ch
+l=d.geZ()
+l.E(0,C.au)
+d.a.toString
+n=t==null?a0:t.M(l)
+k=n
+if(k==null)k=x.CW
+v.E(0,C.aL)
+d.a.toString
+n=t==null?a0:t.M(v)
+j=n
+if(j==null)j=A.aA(31,s.gq(s)>>>16&255,s.gq(s)>>>8&255,s.gq(s)&255)
+u.E(0,C.aL)
+d.a.toString
+t=t==null?a0:t.M(u)
+i=t
+if(i==null)i=A.aA(31,s.gq(s)>>>16&255,s.gq(s)>>>8&255,s.gq(s)&255)
+t=d.a
+n=t.c
+h=t.ay
+g=t.dx
+t=t.fr
+f=d.d
+f.sbT(0,A.b(d.iH$,"_position"))
+f.sQx(A.b(d.qP$,"_reaction"))
+f.sQz(A.b(d.qR$,"_reactionFocusFade"))
+f.sQA(A.b(d.qQ$,"_reactionHoverFade"))
+f.sPf(i)
+f.sQy(j)
+f.sl7(k)
+f.sl3(m)
+d.a.toString
+e=w.f
+f.sGv(e==null?20:e)
+f.sOc(d.nX$)
+f.swG(d.geZ().v(0,C.aw))
+f.sPx(d.geZ().v(0,C.au))
+f.sMS(s)
+f.sPe(r)
+f.saaf(d.a.x)
+f.safB(d.a.y)
+f.saeB(d.a.z)
+f.safQ(d.a.Q)
+f.saag(q)
+f.saeC(p)
+f.sm7(A.a_b(a2))
+f.saeS(d.gh5()!=null)
+f.sahw(d.a.fr.a-40)
+e=a2.X(y.I)
+e.toString
+f.sbE(0,e.f)
+f.sXs(x.as.cy)
+return A.bH(a0,a0,A.ml(a0,d.Ng(!1,g,new A.cF(new B.amt(d,w),y.g),f,t),h,!0,a0,a0,a0,a0,d.ga8T(),d.ga8V(),d.ga8X(),a0,a0,a0,a0,a0,a0,a0,a0,a0,a0,a0,a0),!1,a0,a0,!1,a0,a0,a0,a0,a0,a0,a0,a0,a0,a0,a0,a0,a0,a0,a0,a0,a0,a0,n,a0)}}
+B.Fr.prototype={
+saaf(d){return},
+safB(d){return},
+saeB(d){return},
+safQ(d){return},
+saag(d){if(d.k(0,this.fr))return
+this.fr=d
+this.a3()},
+saeC(d){if(d.k(0,this.fx))return
+this.fx=d
+this.a3()},
+sm7(d){if(d.k(0,this.fy))return
+this.fy=d
+this.a3()},
+sbE(d,e){if(this.go===e)return
+this.go=e
+this.a3()},
+sXs(d){if(d.k(0,this.id))return
+this.id=d
+this.a3()},
+saeS(d){if(d===this.k1)return
+this.k1=d
+this.a3()},
+sahw(d){if(d===this.k2)return
+this.k2=d
+this.a3()},
+a2K(){if(!this.p2)this.a3()},
+aB(d,e){var x,w,v,u,t,s,r,q,p,o,n,m,l=this,k=l.k1
+k.toString
+x=l.a
+w=x.gq(x)
+switch(l.go.a){case 0:v=1-w
+break
+case 1:v=w
+break
+default:v=null}x=l.fx
+x.toString
+u=l.fr
+u.toString
+u=A.D(x,u,w)
+u.toString
+x=l.f
+x.toString
+t=l.e
+t.toString
+t=A.D(x,t,w)
+t.toString
+x=l.id
+x.toString
+s=A.a1L(t,x)
+if(k)r=w<0.5?l.dx:l.cy
+else r=l.dx
+if(k)q=w<0.5?l.dy:l.db
+else q=l.dy
+p=$.aU()?A.be():new A.ba(new A.bb())
+p.sao(0,u)
+o=(e.a-33)/2
+k=e.b
+n=(k-14)/2
+x=l.k2
+x.toString
+m=o-3+v*x
+d.cE(0,A.Al(new A.A(o,n,o+33,n+14),C.YZ),p)
+l.Qf(d,new A.k(m+10,k/2))
+l.a6x(new A.k(m,n-3),d,w,s,r,q)},
+a6x(d,e,f,g,h,i){var x,w,v,u,t=this,s=null
+try{t.p2=!0
+if(t.p1!=null)if(g.k(0,t.k3))u=!1
+else u=!0
+else u=!0
+if(u){t.k3=g
+t.k4=h
+t.ok=i
+u=t.p1
+if(u!=null)u.l(0)
+t.p1=new A.uS(new A.bK(g,s,s,s,C.t9,s,C.aR),t.ga2J())}u=t.p1
+u.toString
+x=u
+w=1-Math.abs(f-0.5)*2
+v=10-w
+u=v*2
+x.j_(e,d.S(0,new A.k(0,w)),t.fy.Cy(new A.F(u,u)))}finally{t.p2=!1}},
+l(d){var x=this,w=x.p1
+if(w!=null)w.l(0)
+x.ok=x.k4=x.k3=x.p1=null
+x.Vq(0)}}
+B.Gf.prototype={
+aY(){this.bq()
+this.bi()
+this.dR()},
+l(d){var x=this,w=x.aN$
+if(w!=null)w.J(0,x.gdE())
+x.aN$=null
+x.au(0)}}
+B.Gg.prototype={
+ac(){var x,w=this,v=null
+w.av()
+x=A.aJ(v,C.M,v,!w.a.c?0:1,w)
+w.iG$=x
+w.iH$=A.b_(C.aI,A.b(x,"_positionController"),C.d5)
+x=A.aJ(v,C.aJ,v,v,w)
+w.jM$=x
+w.qP$=A.b_(C.ag,A.b(x,"_reactionController"),v)
+x=A.aJ(v,C.cB,v,w.l1$||w.l0$?1:0,w)
+w.nV$=x
+w.qQ$=A.b_(C.ag,A.b(x,"_reactionHoverFadeController"),v)
+x=A.aJ(v,C.cB,v,w.l1$||w.l0$?1:0,w)
+w.nW$=x
+w.qR$=A.b_(C.ag,A.b(x,"_reactionFocusFadeController"),v)},
+l(d){var x=this
+A.b(x.iG$,"_positionController").l(0)
+A.b(x.jM$,"_reactionController").l(0)
+A.b(x.nV$,"_reactionHoverFadeController").l(0)
+A.b(x.nW$,"_reactionFocusFadeController").l(0)
+x.WU(0)}}
+B.M0.prototype={
+cY(){return A.aq(["id",this.a],y.N,y.z)}}
+B.KR.prototype={
+cY(){var x=C.hp.h(0,this.b)
+x.toString
+return A.aq(["vendor",this.a,"status",x,"gatewaySerialNumber",this.c],y.N,y.z)}}
+B.a2K.prototype={
+cY(){var x=this,w=C.hp.h(0,x.b)
+w.toString
+return A.aq(["vendor",x.a,"status",w,"gatewaySerialNumber",x.c,"id",x.d],y.N,y.z)}}
+B.r9.prototype={
+V(){return new B.TP(new A.lf(C.cn,$.aW()),new A.b5(null,y.w),C.k)}}
+B.TP.prototype={
+tV(){var x=0,w=A.N(y.D),v,u=this,t,s,r,q
+var $async$tV=A.O(function(d,e){if(d===1)return A.K(e,w)
+while(true)switch(x){case 0:if(u.a.d==null){v=A.d_(null,y.D)
+x=1
+break}t=u.geG()
+t=$.j6().cJ(0,A.cD(t,!1))
+s=u.geG()
+s=$.fR().cJ(0,A.cD(s,!1))
+r=u.a.d
+r.toString
+x=3
+return A.V(J.j8(t,s.vY(new B.M0(r)),y.L),$async$tV)
+case 3:q=e
+u.O(new B.ak_(u,q))
+v=q.a
+x=1
+break
+case 1:return A.L(v,w)}})
+return A.M($async$tV,w)},
+ac(){this.w=this.tV()
+this.av()},
+I(d,e){return new A.et(C.ir,A.a5N(new B.ak5(this),A.b(this.w,"_future"),y.D),null)}}
+B.xC.prototype={
+V(){return new B.TQ(C.k)}}
+B.TQ.prototype={
+lI(){var x=0,w=A.N(y.n),v,u=this,t,s
+var $async$lI=A.O(function(d,e){if(d===1)return A.K(e,w)
+while(true)switch(x){case 0:s=u.geG()
+s=$.j6().cJ(0,A.cD(s,!1))
+t=u.geG()
+x=3
+return A.V(J.j8(s,$.fR().cJ(0,A.cD(t,!1)).w1(new A.rD(u.a.d)),y.n),$async$lI)
+case 3:t=e.a
+v=t==null?A.a([],y.A):t
+x=1
+break
+case 1:return A.L(v,w)}})
+return A.M($async$lI,w)},
+ac(){this.w=this.lI()
+this.av()},
+I(d,e){return A.m3(A.a([A.xW(D.a3I,new B.ake(this,e)),A.a5N(new B.akf(this),A.b(this.w,"_future"),y.n)],y.p),C.O,C.bo,C.Q)}}
+B.yl.prototype={
+V(){return new B.UD(C.k)}}
+B.UD.prototype={
+pt(){var x=0,w=A.N(y.f),v,u=this,t,s
+var $async$pt=A.O(function(d,e){if(d===1)return A.K(e,w)
+while(true)switch(x){case 0:s=u.geG()
+s=$.j6().cJ(0,A.cD(s,!1))
+t=u.geG()
+x=3
+return A.V(J.j8(s,$.fR().cJ(0,A.cD(t,!1)).oK(new A.rD(u.a.d)),y.R),$async$pt)
+case 3:v=e.a
+x=1
+break
+case 1:return A.L(v,w)}})
+return A.M($async$pt,w)},
+ac(){this.w=this.pt()
+this.av()},
+I(d,e){return A.aub(A.a5N(new B.al7(this),A.b(this.w,"_future"),y.f),null)}}
+B.ov.prototype={
+kO(d,e,f){return new A.xu(new B.yl(this.e,null),"Gateway details",null)}}
+B.LM.prototype={}
+var z=a.updateTypes(["~()","~(fC)","~(fD)","~(fc)","~(z?)"])
+B.amr.prototype={
+$1(d){if(d.v(0,C.R))return this.a.a.r
+if(d.v(0,C.aX))return this.a.a.e
+return this.a.a.r},
+$S:52}
+B.amo.prototype={
+$1(d){if(d.v(0,C.R))return this.a?C.f1:C.f5
+if(d.v(0,C.aX))return this.b.p3
+return this.a?C.f5:C.op},
+$S:33}
+B.ams.prototype={
+$1(d){if(d.v(0,C.R))return this.a.a.w
+if(d.v(0,C.aX))return this.a.a.f
+return this.a.a.w},
+$S:52}
+B.amp.prototype={
+$1(d){var x,w
+if(d.v(0,C.R))return this.b?C.ii:C.b8
+if(d.v(0,C.aX)){d.E(0,C.aX)
+x=this.a
+w=x.gBM().a.$1(d)
+if(w==null)w=x.gzJ().a.$1(d)
+return A.aA(128,w.gq(w)>>>16&255,w.gq(w)>>>8&255,w.gq(w)&255)}return this.b?D.Hg:D.Hi},
+$S:33}
+B.amq.prototype={
+$0(){this.a.e=!0},
+$S:0}
+B.amt.prototype={
+$1(d){var x=A.d2(this.a.a.ch,d,y.h)
+if(x==null)x=null
+return x==null?A.d2(C.dL,d,y.Y):x},
+$S:167}
+B.ak_.prototype={
+$0(){var x,w=this.b.a
+if(w!=null){x=this.a
+x.y=w.d===C.f7
+x.x.scl(0,w.b)}},
+$S:0}
+B.ak5.prototype={
+$2(d,e){var x,w,v,u,t,s,r=null
+if(e.b!=null||e.a===C.dU){x=this.a
+w=A.cU(x.a.d==null?"Create device":"Edit device",r,r,r,r,r,r)
+v=A.Cf(x.x,D.Ja,!1,A.ark())
+u=x.y
+t=y.p
+u=A.dH(A.a([A.cU(u?"online":"offline",r,r,r,r,r,r),new B.RI(u,new B.ak3(x),r)],t),C.O,C.bo,C.Q,r)
+s=x.z?r:new B.ak4(x,e,d)
+return A.atq(A.m3(A.a([w,v,u,A.xW(A.cU(x.a.d==null?"Create":"Update",r,r,r,r,r,r),s)],t),C.O,C.bo,C.Q),x.Q)}return C.o6},
+$S:524}
+B.ak3.prototype={
+$1(d){var x=this.a
+x.O(new B.ak2(x,d))},
+$S:12}
+B.ak2.prototype={
+$0(){this.a.y=this.b},
+$S:0}
+B.ak4.prototype={
+$0(){var x=0,w=A.N(y.H),v=this,u,t,s,r,q,p
+var $async$$0=A.O(function(d,e){if(d===1)return A.K(e,w)
+while(true)switch(x){case 0:p=v.a
+p.O(new B.ak0(p))
+x=p.Q.ga9().t4()?2:3
+break
+case 2:u=p.x
+if(p.a.d==null){t=p.geG()
+t=$.fR().cJ(0,A.cD(t,!1))
+u=u.a.a
+s=p.y?C.f7:C.il
+r=p.a.e
+r.toString
+q=t.vX(new B.KR(u,s,r))}else{t=p.geG()
+t=$.fR().cJ(0,A.cD(t,!1))
+s=p.a.d
+s.toString
+u=u.a.a
+r=p.y?C.f7:C.il
+q=t.vZ(new B.a2K(s,u,r,v.b.b.e))}u=p.geG()
+x=4
+return A.V(J.j8($.j6().cJ(0,A.cD(u,!1)),q,y.L),$async$$0)
+case 4:if(e.a!=null)A.afP(v.c,!1).f.my(0,!0)
+case 3:p.O(new B.ak1(p))
+return A.L(null,w)}})
+return A.M($async$$0,w)},
+$S:10}
+B.ak0.prototype={
+$0(){this.a.z=!0},
+$S:0}
+B.ak1.prototype={
+$0(){this.a.z=!1},
+$S:0}
+B.ake.prototype={
+$0(){var x=0,w=A.N(y.H),v=this,u
+var $async$$0=A.O(function(d,e){if(d===1)return A.K(e,w)
+while(true)switch(x){case 0:u=v.a
+x=2
+return A.V(A.asd(new B.akc(u),D.nK,v.b,y.u),$async$$0)
+case 2:if(e===!0)u.O(new B.akd(u))
+return A.L(null,w)}})
+return A.M($async$$0,w)},
+$S:10}
+B.akc.prototype={
+$1(d){var x=null
+return A.bZ(new B.r9(x,this.a.a.d,x),x,x)},
+$S:65}
+B.akd.prototype={
+$0(){var x=this.a
+x.w=x.lI()},
+$S:0}
+B.akf.prototype={
+$2(d,e){var x=null,w=A.a([],y.j),v=e.b
+if(v!=null)w=J.lS(v,new B.akb(this.a,d),y.b).ei(0)
+if(w.length===0)return A.c6(x,x,C.r,x,x,x,x,x,x,x,x,x,x)
+return A.axM(D.T0,w)},
+$S:525}
+B.akb.prototype={
+$1(d){var x=null,w=this.a
+return A.axL(A.a([A.oa(A.cU(d.b,x,x,x,x,x,x)),A.oa(A.cU(d.d.b,x,x,x,x,x,x)),A.oa(A.cU(A.a2k(x).qX(0,d.c),x,x,x,x,x,x)),A.oa(A.dH(A.a([A.yt(x,!0,C.oW,x,new B.ak9(w,this.b,d),C.c3,20,x),A.yt(x,!0,C.oV,x,new B.aka(w,d),C.c3,20,x)],y.p),C.O,C.bo,C.Q,x))],y.F))},
+$S:526}
+B.ak9.prototype={
+$0(){var x=0,w=A.N(y.H),v=this,u
+var $async$$0=A.O(function(d,e){if(d===1)return A.K(e,w)
+while(true)switch(x){case 0:x=2
+return A.V(A.asd(new B.ak7(v.c),D.nK,v.b,y.u),$async$$0)
+case 2:if(e===!0){u=v.a
+u.O(new B.ak8(u))}return A.L(null,w)}})
+return A.M($async$$0,w)},
+$S:10}
+B.ak7.prototype={
+$1(d){var x=null
+return A.bZ(new B.r9(this.a.a,x,x),x,x)},
+$S:65}
+B.ak8.prototype={
+$0(){var x=this.a
+x.w=x.lI()},
+$S:0}
+B.aka.prototype={
+$0(){var x=0,w=A.N(y.H),v=this,u,t,s
+var $async$$0=A.O(function(d,e){if(d===1)return A.K(e,w)
+while(true)switch(x){case 0:t=v.a
+s=t.geG()
+s=$.j6().cJ(0,A.cD(s,!1))
+u=t.geG()
+x=2
+return A.V(J.j8(s,$.fR().cJ(0,A.cD(u,!1)).w0(new B.M0(v.b.a)),y.L),$async$$0)
+case 2:if(e.a!=null)t.O(new B.ak6(t))
+return A.L(null,w)}})
+return A.M($async$$0,w)},
+$S:10}
+B.ak6.prototype={
+$0(){var x=this.a
+x.w=x.lI()},
+$S:0}
+B.al7.prototype={
+$2(d,e){var x,w,v=null,u=e.b
+if(u!=null||e.a===C.dU){x=this.a
+w=y.p
+return A.bZ(A.m3(A.a([A.dH(A.a([A.xW(D.a3U,new B.al5(x,d)),A.RS(D.a3J,new B.al6(d),v)],w),C.O,C.aF,C.Q,v),C.n4,A.dH(A.a([D.a3R,A.cU(u.b,v,v,v,v,v,v)],w),C.O,C.aF,C.Q,v),A.dH(A.a([D.a3S,A.cU(u.c,v,v,v,v,v,v)],w),C.O,C.aF,C.Q,v),C.n4,new B.xC(x.a.d,v)],w),C.O,C.bo,C.Q),v,v)}return A.c6(v,v,C.r,v,v,v,v,v,v,v,v,v,v)},
+$S:115}
+B.al5.prototype={
+$0(){var x=0,w=A.N(y.H),v=this,u
+var $async$$0=A.O(function(d,e){if(d===1)return A.K(e,w)
+while(true)switch(x){case 0:u=v.a
+x=2
+return A.V(A.asd(new B.al3(u),C.nL,v.b,y.u),$async$$0)
+case 2:if(e===!0)u.O(new B.al4(u))
+return A.L(null,w)}})
+return A.M($async$$0,w)},
+$S:10}
+B.al3.prototype={
+$1(d){return A.bZ(new A.rx(this.a.a.d,null),null,null)},
+$S:65}
+B.al4.prototype={
+$0(){var x=this.a
+x.w=x.pt()},
+$S:0}
+B.al6.prototype={
+$0(){var x=A.afP(this.a,!1).f,w=A.a([D.IT],y.m),v=x.IY(C.c.gK(w))
+C.c.sp(v.b,0)
+A.b(x.fy,"_navigationHistory").a=!0
+v.a7f(w,null)},
+$S:0};(function aliases(){var x=B.Gf.prototype
+x.WU=x.l
+x=B.Gg.prototype
+x.WV=x.l})();(function installTearOffs(){var x=a._instance_1u,w=a._instance_0u,v=a._instance_0i
+var u
+x(u=B.E6.prototype,"ga8V","a8W",1)
+x(u,"ga8X","a8Y",2)
+x(u,"ga8T","a8U",3)
+x(u,"ga2E","a2F",4)
+w(u=B.Fr.prototype,"ga2J","a2K",0)
+v(u,"gdU","l",0)})();(function inheritance(){var x=a.mixinHard,w=a.inherit,v=a.inheritMany
+w(B.Yc,A.Dm)
+w(B.RI,A.bd)
+w(B.E5,A.P)
+w(B.Gf,A.X)
+w(B.Gg,B.Gf)
+w(B.E6,B.Gg)
+v(A.cg,[B.amr,B.amo,B.ams,B.amp,B.amt,B.ak3,B.akc,B.akb,B.ak7,B.al3])
+v(A.xb,[B.amq,B.ak_,B.ak2,B.ak4,B.ak0,B.ak1,B.ake,B.akd,B.ak9,B.ak8,B.aka,B.ak6,B.al5,B.al4,B.al6])
+w(B.Fr,A.pQ)
+v(A.H,[B.M0,B.KR])
+w(B.a2K,B.KR)
+v(A.xf,[B.r9,B.xC,B.yl])
+v(A.r0,[B.TP,B.TQ,B.UD])
+v(A.xc,[B.ak5,B.akf,B.al7])
+w(B.ov,A.r1)
+w(B.LM,A.kR)
+x(B.Gf,A.dd)
+x(B.Gg,A.uB)})()
+A.aBi(b.typeUniverse,JSON.parse('{"E5":{"P":[],"h":[]},"Yc":{"J":[]},"RI":{"bd":[],"h":[]},"E6":{"X":["E5"]},"Fr":{"ai":[]},"r9":{"P":[],"h":[]},"TP":{"X":["r9"]},"xC":{"P":[],"h":[]},"TQ":{"X":["xC"]},"yl":{"P":[],"h":[]},"UD":{"X":["yl"]},"ov":{"P":[],"h":[]},"LM":{"kR":["~"]},"aNb":{"aX":[],"aL":[],"h":[]}}'))
+var y=(function rtii(){var x=A.a3
+return{b:x("fT"),L:x("eo"),I:x("fY"),R:x("dx"),F:x("p<xv>"),j:x("p<fT>"),A:x("p<eo>"),m:x("p<kR<@>>"),p:x("p<h>"),w:x("b5<rv>"),n:x("v<eo>"),Y:x("c9"),N:x("i"),y:x("aNb"),t:x("cF<Y>"),g:x("cF<c9>"),s:x("cF<Y?>"),z:x("@"),D:x("eo?"),f:x("dx?"),h:x("c9?"),u:x("z?"),H:x("~")}})();(function constants(){var x=a.makeConstList
+D.nK=new A.aB(0,300,0,200)
+D.Hg=new A.Y(1308622847)
+D.Hi=new A.Y(1375731712)
+D.IT=new B.LM("GatewayListRoute","/",null,C.T)
+D.D7=new A.cT("Vendor",null,null,null,null,null,null,null,null)
+D.Ja=new A.kx(null,null,D.D7,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,!1,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,!0,null,null,null)
+D.HP=new A.ie(D.D7)
+D.a3L=new A.cT("Status",null,null,null,null,null,null,null,null)
+D.HQ=new A.ie(D.a3L)
+D.a3V=new A.cT("Date",null,null,null,null,null,null,null,null)
+D.HO=new A.ie(D.a3V)
+D.T0=A.a(x([D.HP,D.HQ,D.HO,C.ov]),A.a3("p<ie>"))
+D.a_F=new A.F(59,40)
+D.a_G=new A.F(59,48)
+D.a3I=new A.cT("Add device",null,null,null,null,null,null,null,null)
+D.a3J=new A.cT("Back to gateway list",null,null,null,null,null,null,null,null)
+D.a3R=new A.cT("Name: ",null,C.hF,null,null,null,null,null,null)
+D.a3S=new A.cT("Address: ",null,C.hF,null,null,null,null,null,null)
+D.a3U=new A.cT("Update gateway data",null,null,null,null,null,null,null,null)
+D.a6Q=new B.Yc(0,"material")})()}
+$__dart_deferred_initializers__["OPkD+A30dRVTeoiqj/99zZR/dAc="] = $__dart_deferred_initializers__.current
