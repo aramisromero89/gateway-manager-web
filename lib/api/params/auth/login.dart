4a561510f57fef7d7c483ba6eb6174f7374e7c0c
Aramis Romero Carballea <aramisromero89@gmail.com>
2022-10-11 03:33:48 -0400

    add auth dtos

---
+++ lib/api/params/auth/login.dart
@@ -0,0 +1,13 @@
+import 'package:json_annotation/json_annotation.dart';
+part "login.g.dart";
+
+@JsonSerializable()
+class Login {
+  String username;
+  String password;
+
+  Login({required this.username, required this.password});
+
+  factory Login.fromJson(Map<String, dynamic> json) => _$LoginFromJson(json);
+  Map<String, dynamic> toJson() => _$LoginToJson(this);
+}
